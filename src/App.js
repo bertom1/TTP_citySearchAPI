8a61ff72fd38fc4ea4f15880cb5f9e3c3c096c92
Roberto C Melchor <rmelchor151@gmail.com>
2021-01-13 21:41:28 -0500

    add title and styling

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import City from './components/City'
 function App() {
   return (
     <div className="App">
+      <div className = "title"><p>City Search</p></div>
       <City />
     </div>
   );
